be229108411d08e79c197c4f8c766ff05764a357
David Dérus <david.derus@kasual.biz>
2015-07-06 17:58:23 +0200

    [+] Better accessors namespacing

--- lib/model_strength/acts_as_strength.rb
+++ lib/model_strength/acts_as_strength.rb
@@ -7,13 +7,14 @@ module ModelStrength
 
     module ClassMethods
       def acts_as_strength(*attributes)
-        cattr_accessor :attributes, :step
+        cattr_accessor :strength_attributes, :strength_step
+
         before_create :compute_score
         before_update :compute_score
 
-        self.attributes = attributes
+        self.strength_attributes = attributes
         nb_attributes = attributes.size
-        self.step = (100/nb_attributes)
+        self.strength_step = (100/nb_attributes)
 
         include ModelStrength::ActsAsStrength::LocalInstanceMethods
       end
@@ -21,8 +22,8 @@ module ModelStrength
 
     module LocalInstanceMethods
       def compute_score
-        value = self.class.attributes.inject(0) do |total, attribute|
-          read_attribute(attribute).blank? ? total : (total + self.class.step)
+        value = self.class.strength_attributes.inject(0) do |total, attribute|
+          read_attribute(attribute).blank? ? total : (total + self.class.strength_step)
         end
 
         write_attribute(:score, value)
