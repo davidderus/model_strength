005a1df2fa3802132871ddd349c4e01da23df50b
David Dérus <david.derus@kasual.biz>
2015-07-06 17:36:36 +0200

    [+] Trying it

--- lib/model_strength.rb
+++ lib/model_strength.rb
@@ -1,2 +1,4 @@
+require 'model_strength/acts_as_strength'
+
 module ModelStrength
 end
--- lib/model_strength/acts_as_strength.rb
+++ lib/model_strength/acts_as_strength.rb
@@ -1,5 +1,5 @@
 module ModelStrength
-  class ActsAsStrength
+  module ActsAsStrength
     extend ActiveSupport::Concern
 
     included do
@@ -7,14 +7,22 @@ module ModelStrength
 
     module ClassMethods
       def acts_as_strength(*attributes)
+        self.attributes = attributes
         nb_attributes = attributes.size
-        step = (100/nb_attributes)
-        attributes.inject(0) do |total, attribute|
-          total += step unless read_attribute(attribute).blank?
+        self.step = (100/nb_attributes)
+
+        include ModelStrength::ActsAsStrength::LocalInstanceMethods
+      end
+    end
+
+    module LocalInstanceMethods
+      def score
+        self.class.attributes.inject(0) do |total, attribute|
+          total += self.class.step unless read_attribute(attribute).blank?
         end
       end
     end
   end
 end
 
-ActiveRecord::Base.send :include, ModelStrength::ActAsStrength
+ActiveRecord::Base.send :include, ModelStrength::ActsAsStrength
