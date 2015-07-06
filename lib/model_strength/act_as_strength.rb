0e0845086e7d49fec883a5d7284af94350482af9
David Dérus <david.derus@kasual.biz>
2015-07-06 16:53:09 +0200

    [+] Default method

--- lib/model_strength/act_as_strength.rb
+++ lib/model_strength/act_as_strength.rb
@@ -1,4 +1,16 @@
 module ModelStrength
   class ActAsStrength
+    extend ActiveSupport::Concern
+
+    included do
+    end
+
+    module ClassMethods
+      def acts_as_strengthable(*attributes)
+        # TODO
+      end
+    end
   end
 end
+
+ActiveRecord::Base.send :include, ModelStrength::ActAsStrength
