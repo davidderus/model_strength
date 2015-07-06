c12cf31a9b336c33dce53141b1523e441aefd011
David Dérus <david.derus@kasual.biz>
2015-07-06 18:23:08 +0200

    [+] Handling weights

--- lib/model_strength/acts_as_strength.rb
+++ lib/model_strength/acts_as_strength.rb
@@ -6,13 +6,14 @@ module ModelStrength
     end
 
     module ClassMethods
-      def acts_as_strength(*attributes)
-        cattr_accessor :strength_attributes, :strength_step
+      def acts_as_strength(*attributes, weights: { 0 => :empty, 30 => :low, 50 => :medium, 70 => :high, 100 => :complete })
+        cattr_accessor :strength_attributes, :strength_step, :strength_weights
 
         before_create :compute_score
         before_update :compute_score
 
         self.strength_attributes = attributes
+        self.strength_weights = weights
         nb_attributes = attributes.size
         self.strength_step = (100/nb_attributes)
 
@@ -21,12 +22,17 @@ module ModelStrength
     end
 
     module LocalInstanceMethods
+
       def compute_score
         value = self.class.strength_attributes.inject(0) do |total, attribute|
           read_attribute(attribute).blank? ? total : (total + self.class.strength_step)
         end
 
         write_attribute(:score, value)
+      end
+
+      def weight
+        self.class.strength_weights.select{ |score, value| score <= self.score }.values.last
       end
     end
   end
