172b439599051cc06009339016813d529268f154
David Dérus <david.derus@kasual.biz>
2015-07-07 12:16:09 +0200

    [+] Allowing usage of a custom key for score (and instead of score)

--- lib/model_strength/acts_as_strength.rb
+++ lib/model_strength/acts_as_strength.rb
@@ -6,17 +6,25 @@ module ModelStrength
     end
 
     module ClassMethods
-      def acts_as_strength(*attributes, exclude: false, statuses: { 0..30 => :ultra_low, 30..50 => :low, 50..70 => :medium, 70..99 => :high, 100 => :complete })
-        cattr_accessor :strength_attributes, :strength_statuses, :strength_exclude
+      def acts_as_strength(*attributes, key: :score, exclude: false, statuses: { 0..30 => :ultra_low, 30..50 => :low, 50..70 => :medium, 70..99 => :high, 100 => :complete })
+        # Class and Instance accessors
+        cattr_accessor :strength_attributes, :strength_statuses, :strength_exclude, :strength_key
 
+        # Active Record Callbacks
         before_create :compute_score
         before_update :compute_score
 
-        self.strength_attributes = attributes.map { |key| key.to_s }
+        # Setting given parameters
+        self.strength_attributes = attributes.map { |attr_key| attr_key.to_s }
         self.strength_statuses = statuses
         self.strength_exclude = exclude
+        self.strength_key = key
 
+        # Including local instance methods
         include ModelStrength::ActsAsStrength::LocalInstanceMethods
+
+        # Adding dynamic method to check key existence
+        define_method("#{key}?") { read_attribute(key).present? }
       end
     end
 
@@ -36,21 +44,17 @@ module ModelStrength
           read_attribute(attribute).present? ? (total + strength_step) : total
         end
 
-        write_attribute(:score, value)
+        write_attribute(self.class.strength_key, value)
       end
 
       def status
-        self.class.strength_statuses.select{ |score, value| score === self.score }.values.last
-      end
-
-      def score?
-        self.score.present?
+        self.class.strength_statuses.select{ |score, value| score === read_attribute(self.class.strength_key) }.values.last
       end
 
       protected
 
       def default_exclude
-        ['id', 'created_at', 'updated_at', 'score']
+        ['id', 'created_at', 'updated_at'] << self.class.strength_key.to_s
       end
     end
   end
