1eea11338abc4e85751755c66fdf81c811f61650
David Dérus <david.derus@kasual.biz>
2015-07-07 11:22:34 +0200

    [+] Adding an exclude option allowing to negate the act as strength

--- lib/model_strength/acts_as_strength.rb
+++ lib/model_strength/acts_as_strength.rb
@@ -6,16 +6,15 @@ module ModelStrength
     end
 
     module ClassMethods
-      def acts_as_strength(*attributes, statuses: { 0..30 => :ultra_low, 30..50 => :low, 50..70 => :medium, 70..99 => :high, 100 => :complete })
-        cattr_accessor :strength_attributes, :strength_step, :strength_statuses
+      def acts_as_strength(*attributes, exclude: false, statuses: { 0..30 => :ultra_low, 30..50 => :low, 50..70 => :medium, 70..99 => :high, 100 => :complete })
+        cattr_accessor :strength_attributes, :strength_statuses, :strength_exclude
 
         before_create :compute_score
         before_update :compute_score
 
-        self.strength_attributes = attributes
+        self.strength_attributes = attributes.map { |key| key.to_s }
         self.strength_statuses = statuses
-        nb_attributes = attributes.size
-        self.strength_step = (100/nb_attributes)
+        self.strength_exclude = exclude
 
         include ModelStrength::ActsAsStrength::LocalInstanceMethods
       end
@@ -24,8 +23,17 @@ module ModelStrength
     module LocalInstanceMethods
 
       def compute_score
-        value = self.class.strength_attributes.inject(0) do |total, attribute|
-          read_attribute(attribute).present? ? (total + self.class.strength_step) : total
+        if self.class.strength_exclude
+          attributes = self.attributes.except(*(self.class.strength_attributes | default_exclude)).keys
+        else
+          attributes = self.class.strength_attributes
+        end
+
+        nb_attributes = attributes.size
+        strength_step = (100/nb_attributes)
+
+        value = attributes.inject(0) do |total, attribute|
+          read_attribute(attribute).present? ? (total + strength_step) : total
         end
 
         write_attribute(:score, value)
@@ -33,6 +41,12 @@ module ModelStrength
 
       def status
         self.class.strength_statuses.select{ |score, value| score === self.score }.values.last
+      end
+
+      protected
+
+      def default_exclude
+        ['id', 'created_at', 'updated_at', 'score']
       end
     end
   end
--- test/dummy/app/models/product.rb
+++ test/dummy/app/models/product.rb
@@ -1,3 +1,3 @@
 class Product < ActiveRecord::Base
-  acts_as_strength :name, :description
+  acts_as_strength :description, :note, exclude: true
 end
